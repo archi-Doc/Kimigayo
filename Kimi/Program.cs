c60e7a7b5502eaae169a34a6d51cc812db0b5658
archi-Doc <archidoc422@gmail.com>
2026-05-19 17:05:13 +0900

    .

---
+++ Kimi/Program.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Kimi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## Kimigayo
+Work in progress
+
