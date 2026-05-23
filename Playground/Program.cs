1c33ed22702a51b531cab198d97ff8e02f16ae90
archi-Doc <archidoc422@gmail.com>
2026-05-23 18:04:39 +0900

    .

---
+++ Playground/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Playground;
+
+using Arc.Unit;
+using Kimigayo;
+
+public class Builder
+{
+    public IConsoleService ConsoleService { get; set; }
+
+    public Builder()
+    {
+        this.ConsoleService = SimplePrompt.
+    }
+}
+
+public class KimiParser
+{
+    public KimiParser()
+    {
+    }
+
+    public CodeTree TryParse(ReadOnlySpan<char> text)
+    {
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var tree = CodeTree.Parse("");
+    }
+}
