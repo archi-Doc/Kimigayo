cb77f4ebeff7f9c3d7d8ebe17b9a2d46b0f6170c
archi-Doc <archidoc422@gmail.com>
2026-05-30 18:30:57 +0900

    .

---
+++ Kimi/Constants.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Kimigayo;
+
+public static class Constants
+{
+    public const int IndentationSpaces = 4;
+    public const char SpaceChar = ' ';
+}
--- Kimi/Project/Reader.cs
+++ Kimi/Project/Reader.cs
@@ -2,7 +2,7 @@
 
 namespace Kimigayo;
 
-internal enum ReaderContext : byte
+internal enum ReaderMode : byte
 {
     Global,
 }
@@ -16,28 +16,44 @@ internal enum ReaderToken : byte
     Eof,
 }
 
+internal static class ReaderHelper
+{
+    public static bool IsLineContext(this ReaderMode readerContext) => readerContext switch
+    {
+        ReaderMode.Global => true,
+        _ => false,
+    };
+}
+
 internal ref struct Reader
 {
     #region FieldAndProperty
 
-    private readonly int indentationSpaces = 4;
-    private ReadOnlySpan<char> text;
+    private ReadOnlySpan<char> span;
 
-    public ReaderContext Current { get; private set; }
+    public ReaderMode CurrentMode { get; private set; }
 
     #endregion
 
     public Reader(KimiControl kimiControl, ReadOnlySpan<char> text)
     {
-        this.text = text;
+        this.span = text;
     }
 
     public void Read(out ReaderToken token, out ReadOnlySpan<char> text)
     {
-        var span = this.text;
-
-        if (char.IsWhiteSpace(span[0]))
+        var numberOfSpaces = Arc.BaseHelper.CountLeadingSpaces(this.span);
+        if (numberOfSpaces > 0)
         {// Space
+            if (this.CurrentMode.IsLineContext())
+            {
+
+
+            }
+            else
+            {
+                this.span = this.span.Slice(numberOfSpaces);
+            }
 
         }
 
