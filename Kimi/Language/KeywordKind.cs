1f278902ea6697d3a4737b859affbf19a51913de
archi-Doc <archidoc422@gmail.com>
2026-06-01 20:40:05 +0900

    .

--- Kimi/Language/KeywordKind.cs
+++ Kimi/Language/KeywordKind.cs
@@ -5,14 +5,32 @@ namespace Kimigayo.Language;
 public enum KeywordKind : byte
 {
     // Primitive types
+    Bool,
+    Isize,
+    Usize,
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+    F32,
+    F64,
+
+    // Other
     Namespace = 32,
     Use,
     Group,
     Struct,
     Enum,
+    Const,
     Public,
     Protected,
-    Private,
+    // Private,
     Internal,
     Protected_or_internal,
     Protected_and_internal,
--- Kimi/Language/LanguageHelper.cs
+++ Kimi/Language/LanguageHelper.cs
@@ -4,4 +4,22 @@ namespace Kimigayo.Language;
 
 public static class LanguageHelper
 {
+    public static IReadOnlyDictionary<KeywordKind, string> KeywordKindToKeyword => _keywordKindToKeyword;
+
+    public static IReadOnlyDictionary<string, KeywordKind> KeywordToKeywordKind => _keywordToKeywordKind;
+
+    private static readonly Dictionary<KeywordKind, string> _keywordKindToKeyword;
+    private static readonly Dictionary<string, KeywordKind> _keywordToKeywordKind;
+
+    static LanguageHelper()
+    {
+        _keywordKindToKeyword = new();
+        _keywordToKeywordKind = new();
+        foreach (var x in Enum.GetValues<KeywordKind>())
+        {
+            var keyword = x.ToString().ToLower();
+            _keywordKindToKeyword[x] = keyword;
+            _keywordToKeywordKind[keyword] = x;
+        }
+    }
 }
