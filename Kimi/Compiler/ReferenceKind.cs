f8a4f001f6ef3c71f6752544b8ac289d725f86f8
archi-Doc <archidoc422@gmail.com>
2026-07-22 16:55:43 +0900

    .

--- Kimi/Compiler/ReferenceKind.cs
+++ Kimi/Compiler/ReferenceKind.cs
@@ -6,8 +6,8 @@ public enum ReferenceKind : byte
 {
     None,
     Ref, // "ref" In-stack reference
-    Amp, // "&" In-scope reference
-    Own, // "&own" Owner reference
+    Borrow, // "&" In-scope reference
+    Owner, // "&own" Owner reference
     Unsafe, // "&unsafe" Unsafe reference
     Rc, // "&rc" Out-scope reference with counter
     Arc, // "&arc" Out-thread reference with atomic counter
