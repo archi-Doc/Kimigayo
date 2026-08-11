b68ab4c2645bfc65dd999f5afe665ebf9e19cb62
archi-Doc <archidoc422@gmail.com>
2026-08-11 22:22:25 +0900

    ...

---
+++ Kimi/Misc/Target/Architecture.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kimi.Misc.Target;
+
+public enum Architecture
+{
+    Unknown,
+    X86,        // i386 系 32bit
+    X86_64,
+    Arm,        // 32bit ARM (LE)
+    ArmBe,      // 32bit ARM (BE)
+    AArch64,
+    AArch64Be,
+    RiscV32,
+    RiscV64,
+    Wasm32,
+    Wasm64,
+    Mips,       // 32bit BE
+    MipsEl,     // 32bit LE
+    Mips64,     // 64bit BE
+    Mips64El,   // 64bit LE
+    PowerPc64,
+    PowerPc64Le,
+    Sparcv9,
+    SystemZ,    // s390x
+}
