6d35ee6ce7b8ae5c1baba9d3419b5dd49f7ecf41
archi-Doc <archidoc422@gmail.com>
2026-08-18 21:37:44 +0900

    .

---
+++ Kimi/Diagnostics/DiagnosticEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tinyhand;
+
+namespace Kimi.Diagnostics;
+
+[TinyhandObject(ImplicitMemberNameAsKey = true, EnumAsString = true)]
+[ValueLinkObject]
+public partial record class DiagnosticEntry
+{
+    [Link(Primary = true, Unique = true, Type = ChainType.Unordered)]
+    public string Name { get; init; } = string.Empty;
+
+    public DiagnosticSeverity Severity { get; init; } = DiagnosticSeverity.Error;
+
+    public string Message { get; init; } = string.Empty;
+
+    public string? Label { get; init; }
+
+    public string? Fix { get; init; }
+
+    public string? Note { get; init; }
+
+    public DiagnosticEntry(string name, DiagnosticSeverity diagnosticSeverity, string message, string? label = default, string? fix = default, string? note = default)
+    {
+        this.Name = name;
+        this.Severity = diagnosticSeverity;
+        this.Message = message;
+        this.Label = label;
+        this.Fix = fix;
+        this.Note = note;
+    }
+}
