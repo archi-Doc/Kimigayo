b9f5a041ae3fa35496636980aa72c9fd2bac34be
archi-Doc <archidoc422@gmail.com>
2026-08-28 22:12:29 +0900

    .

---
+++ Kimi/Compiler/Parsing/Identifier/Signature.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Collections.Immutable;
+
+namespace Kimi.Compiler.Parsing;
+
+[TinyhandObject]
+public readonly partial record struct TypeSignature(
+    [property: Key(0)] SemanticsKind Semantics,
+    [property: Key(1)] string Name,
+    [property: Key(2)] int GenericParameterCount);
+
+[TinyhandObject]
+public readonly partial record struct FunctionSignature(
+    [property: Key(0)] string Name,
+    [property: Key(1)] int GenericParameterCount,
+    [property: Key(2)] ImmutableArray<ParameterSignature> Parameters);
+
+[TinyhandObject]
+public readonly partial record struct ParameterSignature(
+    [property: Key(0)] TypeKoto TypeKoto);
+
+[TinyhandObject]
+public readonly partial record struct FieldSignature(
+    [property: Key(0)] string Name);
+
+[TinyhandObject]
+public readonly partial record struct PropertySignature(
+    [property: Key(0)] string Name);
