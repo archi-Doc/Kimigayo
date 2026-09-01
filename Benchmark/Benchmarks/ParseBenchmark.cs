0b235aaec17968aa44e341edf185e1d4639a6dba
archi-Doc <archidoc422@gmail.com>
2026-09-02 00:35:31 +0900

    .

--- Benchmark/Benchmarks/ParseBenchmark.cs
+++ Benchmark/Benchmarks/ParseBenchmark.cs
@@ -111,7 +111,7 @@ public class ParseBenchmark
 
     [Benchmark]
     public Koto Test1()
-    {// 9.6 us
+    {// 9.0 us
         var kotonoha = this.compilation.Kotonoha;
         var codeContext = kotonoha.CreateCodeContext();
         codeContext.Parse(kotonoha.RootKoto, this.sourceText);
