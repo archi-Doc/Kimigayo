7b82ef2d09d3c6dc32e5b0c92b54478960708937
archi-Doc <archidoc422@gmail.com>
2026-09-09 15:36:14 +0900

    .

--- Benchmark/Benchmarks/ParseBenchmark.cs
+++ Benchmark/Benchmarks/ParseBenchmark.cs
@@ -60,7 +60,7 @@ public class ParseBenchmark
                     return 1
 
                 func Method2() -> ()
-                    #Condition(Os=="Windows")
+                    #if(Os=="Windows")
                     var i = if (x == true) => 1 else => 0
                     var i2 = if (x == true)
                         1
